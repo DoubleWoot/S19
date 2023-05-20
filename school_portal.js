659e2f66e761b8be7940765eeb2d4bec4a3a4816
DoubleWoot <202110423@feualabang.edu.ph>
2023-05-20 09:22:06 +0800

    Second Commit

--- school_portal.js
+++ school_portal.js
@@ -7,18 +7,18 @@ function login(){
     if(typeof username === "string" && typeof role === "string" && typeof password === "string"){
 
         if(role == "admin"){
-            console.log("Welcome back to the class portal, admin!");
+            alert("Welcome back to the class portal, admin!");
         }
         else if(role == "teacher"){
-            console.log("Thank you for logging in, teacher!");
+            alert("Thank you for logging in, teacher!");
             gradeInput();
         }
         else if(role == "student"){
-            console.log("Welcome to the class portal, student!");
+            alert("Welcome to the class portal, student!");
             gradeInput();
         }
         else{
-            console.log("Role out of range.");
+            alert("Role out of range.");
         }
     }
 }
@@ -37,30 +37,30 @@ function grade(grade1, grade2, grade3, grade4){
 
     if(average >= 96 && average <= 100){
         if(role == "teacher"){
-            console.log("Hello teacher, your average is:", Math.round(average), "\nand your grade is A+");
+            alert("Hello teacher, your average is:", Math.round(average), "\nand your grade is A+");
         }else if(role == "student"){
-            console.log("Hello student, your average is:", Math.round(average), "\nand your grade is A+");
+            alert("Hello student, your average is:", Math.round(average), "\nand your grade is A+");
         }
     }
     else if(average >= 90 && average <= 95){
         if(role == "teacher"){
-            console.log("Hello teacher, your average is:", Math.round(average), "\nand your grade is A");
+            alert("Hello teacher, your average is:", Math.round(average), "\nand your grade is A");
         }else if(role == "student"){
-            console.log("Hello student, your average is:", Math.round(average), "\nand your grade is A");
+            alert("Hello student, your average is:", Math.round(average), "\nand your grade is A");
         }
     }
     else if(average >= 85 && average <= 89){
         if(role == "teacher"){
-            console.log("Hello teacher, your average is:", Math.round(average), "\nand your grade is B");
+            alert("Hello teacher, your average is:", Math.round(average), "\nand your grade is B");
         }else if(role == "student"){
-            console.log("Hello student, your average is:", Math.round(average), "\nand your grade is B");
+            alert("Hello student, your average is:", Math.round(average), "\nand your grade is B");
         }
     }
     else if(average <= 74){
         if(role == "teacher"){
-            console.log("Hello teacher, your average is:", Math.round(average), "\nand your grade is F");
+            alert("Hello teacher, your average is:", Math.round(average), "\nand your grade is F");
         }else if(role == "student"){
-            console.log("Hello student, your average is:", Math.round(average), "\nand your grade is F");
+            alert("Hello student, your average is:", Math.round(average), "\nand your grade is F");
         }
     }
 }
